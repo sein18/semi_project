14da69c1b7f963245837f21a09e0a20ebd0525c1
tpdls7080@naver.com <tpdls7080@naver.com>
2022-06-21 17:52:08 +0900

    카카오 api를 활용하여 애견병원을 나눠서  출력

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/9/204b7eaf8cf0001c1c62d5c561001d12
@@ -0,0 +1,12 @@
+package com.team.dog.model.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.team.dog.model.mapper.LoginMapper;
+
+@Service
+public class LoginService {
+	@Autowired
+	private LoginMapper loginMapper;
+}
--- dog/src/main/java/com/team/dog/model/dto/LoginDto.java
+++ dog/src/main/java/com/team/dog/model/dto/LoginDto.java
@@ -7,10 +7,12 @@ public class LoginDto {
 	private String pw;
 	private String email;
 	private String phone;
+
 	public LoginDto() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public LoginDto(int no, String id, String name, String pw, String email, String phone) {
 		super();
 		this.no = no;
@@ -20,42 +22,59 @@ public class LoginDto {
 		this.email = email;
 		this.phone = phone;
 	}
+
 	public int getNo() {
 		return no;
 	}
+
 	public void setNo(int no) {
 		this.no = no;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getPw() {
 		return pw;
 	}
+
 	public void setPw(String pw) {
 		this.pw = pw;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return "LoginDto [no=" + no + ", id=" + id + ", name=" + name + ", pw=" + pw + ", email=" + email + ", phone="
+				+ phone + "]";
+	}
+
 }
